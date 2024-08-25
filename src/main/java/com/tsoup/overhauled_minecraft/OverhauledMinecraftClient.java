bf383bffab5fe5585a0359643243e789f1088b9f
Timmy Soup <88457199+GodMadeEarth@users.noreply.github.com>
2024-08-25 05:35:08 -0400

    Project Setup

---
+++ src/main/java/com/tsoup/overhauled_minecraft/OverhauledMinecraftClient.java
@@ -0,0 +1,10 @@
+package com.tsoup.overhauled_minecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OverhauledMinecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/tsoup/overhauled_minecraft/OverhauledMinecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tsoup.overhauled_minecraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OverhauledMinecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
